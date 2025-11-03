95e9939bfb25c16a2ba74d4549dcf149531533be
AndreeFrby <88806982+Andres3810@users.noreply.github.com>
2025-11-03 09:49:57 -0300

    Crear config.js
    Creo el archivo config.js para no dejar los datos sensibles en el código Main.

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * ##################################
+ * #      Archivo de Configuración #
+ * ##################################
+ *
+ * acá van todos los datos sensibles y configuraciones
+ * para que no estén mezclados con la lógica del bot.
+ */
+
+module.exports = {
+  // Identidad del bot
+  identity: {
+    username: 'YourBotUsername', // Nombre de la cuenta del bot
+    password: 'oauth:your_oauth_token_here'  // Token OAuth de la cuenta del bot
+  },
+  
+  // Canal al que se unirá el bot, pueden ser varios.
+  channels: [
+    'channelname' // Tu canal principal
+  ],
+
+  // Redes sociales (para el comando !social)
+  social: {
+    twitter: '@tu_x',
+    instagram: '@tu_ig',
+    discord: 'https://discord.gg/discord'
+  }
+};
